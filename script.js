2e26e2efd7a6375ebeba8d0a538948d851a48319
Dhruv Raj Singh <115424327+CAPTAINRAJ19@users.noreply.github.com>
2024-09-06 22:10:12 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 var y=Math.random();
 if(Math.floor(y*6)<1){
-document.querySelector(".img1").setAttribute("src","images/dice1.png");
+document.querySelector(".img1").setAttribute("src","dice1.png");
 }
 else if(Math.floor(y*6)<2){
-document.querySelector(".img1").setAttribute("src","images/dice2.png");
+document.querySelector(".img1").setAttribute("src","dice2.png");
 }
 else if(Math.floor(y*6)<3){
-    document.querySelector(".img1").setAttribute("src","images/dice3.png");
+    document.querySelector(".img1").setAttribute("src","dice3.png");
 }
 else if(Math.floor(y*6)<4){
-    document.querySelector(".img1").setAttribute("src","images/dice4.png");
+    document.querySelector(".img1").setAttribute("src","dice4.png");
 }
 else if(Math.floor(y*6)<5){
-    document.querySelector(".img1").setAttribute("src","images/dice5.png");
+    document.querySelector(".img1").setAttribute("src","dice5.png");
 }
 else {
-    document.querySelector(".img1").setAttribute("src","images/dice6.png");
+    document.querySelector(".img1").setAttribute("src","dice6.png");
 }
 
 
 var x=Math.random();
 if(Math.floor(x*6)<1){
-document.querySelector(".img2").setAttribute("src","images/dice1.png");
+document.querySelector(".img2").setAttribute("src","dice1.png");
 }
 else if(Math.floor(x*6)<2){
-document.querySelector(".img2").setAttribute("src","images/dice2.png");
+document.querySelector(".img2").setAttribute("src","dice2.png");
 }
 else if(Math.floor(x*6)<3){
-    document.querySelector(".img2").setAttribute("src","images/dice3.png");
+    document.querySelector(".img2").setAttribute("src","dice3.png");
 }
 else if(Math.floor(x*6)<4){
-    document.querySelector(".img2").setAttribute("src","images/dice4.png");
+    document.querySelector(".img2").setAttribute("src","dice4.png");
 }
 else if(Math.floor(x*6)<5){
-    document.querySelector(".img2").setAttribute("src","images/dice5.png");
+    document.querySelector(".img2").setAttribute("src","dice5.png");
 }
 else {
-    document.querySelector(".img2").setAttribute("src","images/dice6.png");
+    document.querySelector(".img2").setAttribute("src","dice6.png");
 }
 
 if(Math.floor(x*6)>Math.floor(y*6)){
